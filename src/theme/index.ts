68776b9dafe0e21001a2b4c0bcbe5646df5f8fb3
Stanley Jones <stanley@sunshocked.com>
2022-11-12 21:55:22 -0800

    backup, try something else

--- src/theme/index.ts
+++ src/theme/index.ts
@@ -2,7 +2,7 @@ import { extendTheme } from "@chakra-ui/react"
 
 import { Textarea, Tabs, Button, Heading, Text, Input } from "./components"
 import { colors, fonts, styles } from "./foundation"
-export * from "@chakra-ui/react"
+export { ChakraProvider as ThemeProvider } from "@chakra-ui/react"
 
 export const theme = extendTheme({
   colors,
