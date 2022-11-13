52f8282b92d5f31d3cc44fa2c7b0db403a05936d
Stanley Jones <stanley@sunshocked.com>
2022-11-12 23:21:15 -0800

    and static assets

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,15 @@
-export * from "./helpers"
-export * from "./hooks"
-export * from "./lib"
+// Static assets must be imported first
+import cubePng from "./assets/cube.png"
+import logoSvg from "./assets/logo.svg"
+export { cubePng, logoSvg }
 
 // Export both ChakraUI components and local components
 export * from "./components"
 
 // Explicit re-export to override the ChakraUI theme
 export { ThemeProvider, theme } from "./theme"
+
+// The rest are straightforward
+export * from "./helpers"
+export * from "./hooks"
+export * from "./lib"
