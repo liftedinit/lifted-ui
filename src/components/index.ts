cfb7f60c8b72e1d43a9c1b531cd3e3aaaab3b55c
Stanley Jones <stanley@sunshocked.com>
2022-11-15 15:13:48 -0800

    build: Collapse UIKit into proper places

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,16 +2,19 @@
 export * from "@chakra-ui/react"
 
 // Add local components
+
 export * from "./address-text"
 export * from "./copy-to-clipboard"
 export * from "./data-field"
 export * from "./form"
 export * from "./icons"
 export * from "./nav"
-export * from "./nav-item"
 export * from "./option-card"
 export * from "./steps"
 export * from "./txn-expire-text"
 
-// Explicit re-export to override ChakraUI Modal
+// Explicit re-export to override ChakraUI
+export { AlertDialog } from "./alert-dialog"
+export { Breadcrumb } from "./breadcrumb"
 export { Modal } from "./modal"
+export { useToast } from "./toast"
