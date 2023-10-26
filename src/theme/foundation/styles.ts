07ebf47d492bec7cf8acdb28cc0235a997e1edab
Justin Griffin <jgriffin555@comcast.net>
2023-10-26 16:00:30 -0500

    feat: Add color mode config to global theme and customizing styles for light/dark modes.

--- src/theme/foundation/styles.ts
+++ src/theme/foundation/styles.ts
@@ -1,9 +1,16 @@
+import { mode } from "@chakra-ui/theme-tools";
+
 export const styles = {
-  global: {
+  global: (props: Record<string, any>) => ({ 
     "html,body,#root": {
-      bgColor: "lifted.gray.50",
-      color: "black",
+      bg: mode("lifted.gray.100","lifted.gray.800",)(props),
+      bgColor: mode("lifted.gray.100","lifted.gray.800",)(props),
+      color: mode("black","white")(props),
       height: "100%",
     },
-  },
+    "text": {
+      color: mode("black","white")(props),
+      fill: mode("black","white")(props),
+    }
+  }),
 };
--- src/theme/index.ts
+++ src/theme/index.ts
@@ -1,11 +1,15 @@
 import { extendTheme } from "@chakra-ui/react"
-import { Textarea, Tabs, Button, Heading, Text, Input } from ".."
+import { Textarea, Tabs, Button, Heading, Text, Input, Code } from ".."
 import { colors, fonts, styles } from "./foundation"
 export { ChakraProvider as ThemeProvider } from "@chakra-ui/react"
 
 export const theme = extendTheme({
+  config: {
+    initialColorMode: "light",
+    useSystemColorMode: false,
+  },
   colors,
-  components: { Button, Heading, Input, Tabs, Text, Textarea },
+  components: { Button, Heading, Input, Tabs, Text, Textarea, Code },
   fonts,
   styles,
 })
