5b4454ffe81ebfa6bb25c1e548b087d6893e9ec0
Justin Griffin <jgriffin555@comcast.net>
2023-10-27 07:29:05 -0500

    feat: Testing mode on direct style incluson

--- src/theme/index.ts
+++ src/theme/index.ts
@@ -1,5 +1,6 @@
 import { extendTheme } from "@chakra-ui/react"
 import { Textarea, Tabs, Table, Button, Heading, Text, Input } from ".."
+import { mode } from "@chakra-ui/theme-tools";
 import { colors, fonts, styles } from "./foundation"
 export { ChakraProvider as ThemeProvider } from "@chakra-ui/react"
 
@@ -10,16 +11,16 @@ export const theme = extendTheme({
   },
   colors,
   components: { 
-    Table: {
+    Table: (props: Record<string, any>) => ({
       baseStyle: {
         th: {
-          borderColor: "lifted.gray.200", 
+          borderColor: mode("lifted.gray.200","lifted.gray.700",)(props),
         },
         td: {
-          borderColor: "lifted.gray.200",
+          borderColor: mode("lifted.gray.200","lifted.gray.700",)(props),
         },
       },
-    },
+    }),
   },
   fonts,
   styles,
