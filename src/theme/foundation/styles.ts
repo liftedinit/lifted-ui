d37c81510c6030cc9bbad6f84861ad159c177b0b
Justin Griffin <jgriffin555@comcast.net>
2023-10-27 16:47:23 -0500

    feat: Adding apexcharts tooltip overrides

--- src/theme/foundation/styles.ts
+++ src/theme/foundation/styles.ts
@@ -15,10 +15,18 @@ export const styles = {
     ".table-lifted td": {
       borderBottomColor: mode("lifted.gray.200","lifted.gray.500",)(props),
     },
+    ".apexcharts-tooltip.apexcharts-theme-light .apexcharts-tooltip-title": {
+      bg: mode("lifted.gray.100","lifted.gray.800",)(props),
+      borderBottomColor: mode("black","white")(props),
+    },
     ".apexcharts-tooltip-title": {
       color: mode("black","lifted.gray.500")(props),
     },
-    ".apexcharts-tooltip-ygroup span": {
+    ".apexcharts-tooltip.apexcharts-theme-light" : {
+      bg: mode("lifted.gray.100","lifted.gray.800",)(props),
+      color: mode("black","white")(props),
+    },
+    ".apexcharts-tooltip-text-y-label": {
       color: mode("black","lifted.gray.500")(props),
     },
   }),
