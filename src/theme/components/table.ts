fd687c364f68738e328c362f6b46735539cab436
Justin Griffin <jgriffin555@comcast.net>
2023-10-26 21:01:44 -0500

    feat: Migrate table to imported component override

--- src/theme/components/table.ts
+++ src/theme/components/table.ts
@@ -3,182 +3,66 @@ import { tableAnatomy as parts } from "@chakra-ui/anatomy"
 import { mode } from "@chakra-ui/theme-tools"
 import type {
   PartsStyleFunction,
-  PartsStyleObject,
-  SystemStyleObject,
 } from "@chakra-ui/theme-tools"
 
-const baseStyle: PartsStyleObject<typeof parts> = {
-  table: {
-    fontVariantNumeric: "lining-nums tabular-nums",
-    borderCollapse: "collapse",
-    width: "full",
-  },
-  th: {
-    fontFamily: "heading",
-    fontWeight: "bold",
-    textTransform: "uppercase",
-    letterSpacing: "wider",
-    textAlign: "start",
-  },
-  td: {
-    textAlign: "start",
-  },
-  caption: {
-    mt: 4,
-    fontFamily: "heading",
-    textAlign: "center",
-    fontWeight: "medium",
-  },
-}
 
-const numericStyles: SystemStyleObject = {
-  "&[data-is-numeric=true]": {
-    textAlign: "end",
-  },
-}
 
-const variantSimple: PartsStyleFunction<typeof parts> = (props) => {
+// export const Table: any = (props: any) => ({
+//   variants: {
+//     simple: {
+//       th: {
+//         color: mode("gray.600", "gray.400")(props),
+//         borderBottom: "1px",
+//         borderColor: mode("lifted.gray.100", "lifted.gray.600")(props),
+//       },
+//       td: {
+//         borderBottom: "1px",
+//         borderColor: mode("lifted.gray.100", "lifted.gray.600")(props),
+//       },
+//       caption: {
+//         color: mode("lifted.gray.600", "lifted.gray.100")(props),
+//       },
+//       tfoot: {
+//         tr: {
+//           "&:last-of-type": {
+//             th: { borderBottomWidth: 0 },
+//           },
+//         },
+//       },
+//     },
+//   }
+// });
 
-  return {
+export const Table = {
+  baseStyle: {
     th: {
-      color: mode("gray.600", "gray.400")(props),
-      borderBottom: "1px",
-      borderColor: mode("lifted.gray.100", "lifted.gray.600")(props),
-      ...numericStyles,
+      borderColor: "red.500",
     },
     td: {
-      borderBottom: "1px",
-      borderColor: mode("lifted.gray.100", "lifted.gray.600")(props),
-      ...numericStyles,
+      borderColor: "blue.500",
     },
-    caption: {
-      color: mode("lifted.gray.600", "lifted.gray.100")(props),
-    },
-    tfoot: {
-      tr: {
-        "&:last-of-type": {
-          th: { borderBottomWidth: 0 },
-        },
+  },
+  variants: {
+    simple: (props:any) => ({
+      th: {
+        color: mode("gray.600", "gray.400"),
+        borderBottom: "1px",
+        borderColor: mode("lifted.gray.100", "lifted.gray.600")(props),
       },
-    },
-  }
-}
-
-const variantStripe: PartsStyleFunction<typeof parts> = (props) => {
-  const { colorScheme: c } = props
-
-  return {
-    th: {
-      color: mode("lifted.gray.600", "lifted.gray.400")(props),
-      borderBottom: "1px",
-      borderColor: mode("lifted.gray.100", "lifted.gray.700")(props),
-      ...numericStyles,
-    },
-    td: {
-      borderBottom: "1px",
-      borderColor: mode("lifted.gray.100", "lifted.gray.700")(props),
-      ...numericStyles,
-    },
-    caption: {
-      color: mode("lifted.gray.600", "lifted.gray.100")(props),
-    },
-    tbody: {
-      tr: {
-        "&:nth-of-type(odd)": {
-          "th, td": {
-            borderBottomWidth: "1px",
-            borderColor: mode("lifted.gray.100", "lifted.gray.700")(props),
-          },
-          td: {
-            background: mode("lifted.gray.100", "lifted.gray.700")(props),
+      td: {
+        borderBottom: "1px",
+        borderColor: mode("lifted.gray.100", "lifted.gray.600")(props),
+      },
+      caption: {
+        color: mode("lifted.gray.600", "lifted.gray.100")(props),
+      },
+      tfoot: {
+        tr: {
+          "&:last-of-type": {
+            th: { borderBottomWidth: 0 },
           },
         },
       },
-    },
-    tfoot: {
-      tr: {
-        "&:last-of-type": {
-          th: { borderBottomWidth: 0 },
-        },
-      },
-    },
-  }
-}
-
-const variants = {
-  simple: variantSimple,
-  striped: variantStripe,
-  unstyled: {},
-}
-
-const sizes: Record<string, PartsStyleObject<typeof parts>> = {
-  sm: {
-    th: {
-      px: "4",
-      py: "1",
-      lineHeight: "4",
-      fontSize: "xs",
-    },
-    td: {
-      px: "4",
-      py: "2",
-      fontSize: "sm",
-      lineHeight: "4",
-    },
-    caption: {
-      px: "4",
-      py: "2",
-      fontSize: "xs",
-    },
+    }),
   },
-  md: {
-    th: {
-      px: "6",
-      py: "3",
-      lineHeight: "4",
-      fontSize: "xs",
-    },
-    td: {
-      px: "6",
-      py: "4",
-      lineHeight: "5",
-    },
-    caption: {
-      px: "6",
-      py: "2",
-      fontSize: "sm",
-    },
-  },
-  lg: {
-    th: {
-      px: "8",
-      py: "4",
-      lineHeight: "5",
-      fontSize: "sm",
-    },
-    td: {
-      px: "8",
-      py: "5",
-      lineHeight: "6",
-    },
-    caption: {
-      px: "6",
-      py: "2",
-      fontSize: "md",
-    },
-  },
-}
-
-const defaultProps = {
-  variant: "simple",
-  size: "md",
-  colorScheme: "gray",
-}
-
-export const Table: any = {
-  parts: parts.keys,
-  baseStyle,
-  variants,
-  sizes,
-  defaultProps,
 }
--- src/theme/index.ts
+++ src/theme/index.ts
@@ -1,6 +1,5 @@
 import { extendTheme } from "@chakra-ui/react"
-// import { Textarea, Tabs, Table, Button, Heading, Text, Input } from ".."
-import { mode } from "@chakra-ui/theme-tools"
+import { Textarea, Tabs, Table, Button, Heading, Text, Input } from ".."
 import { colors, fonts, styles } from "./foundation"
 export { ChakraProvider as ThemeProvider } from "@chakra-ui/react"
 
@@ -10,41 +9,7 @@ export const theme = extendTheme({
     useSystemColorMode: false,
   },
   colors,
-  components: {
-    Table: {
-      baseStyle: {
-        th: {
-          borderColor: "red.500",
-        },
-        td: {
-          borderColor: "blue.500",
-        },
-      },
-      variants: {
-        simple: (props:any) => ({
-          th: {
-            color: mode("gray.600", "gray.400"),
-            borderBottom: "1px",
-            borderColor: mode("lifted.gray.100", "lifted.gray.600"),
-          },
-          td: {
-            borderBottom: "1px",
-            borderColor: mode("lifted.gray.100", "lifted.gray.600"),
-          },
-          caption: {
-            color: mode("lifted.gray.600", "lifted.gray.100"),
-          },
-          tfoot: {
-            tr: {
-              "&:last-of-type": {
-                th: { borderBottomWidth: 0 },
-              },
-            },
-          },
-        }),
-      },
-    },
-  },
+  components: { Button, Heading, Input, Table, Tabs, Text, Textarea },
   fonts,
   styles,
 })
