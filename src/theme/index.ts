86020133cbafa01343fdfdc440e8000f77e84c6e
Justin Griffin <jgriffin555@comcast.net>
2023-10-26 19:22:20 -0500

    feat: Testing direct setting Table component

--- src/theme/index.ts
+++ src/theme/index.ts
@@ -1,5 +1,6 @@
 import { extendTheme } from "@chakra-ui/react"
-import { Textarea, Tabs, Table, Button, Heading, Text, Input } from ".."
+// import { Textarea, Tabs, Table, Button, Heading, Text, Input } from ".."
+import { mode } from "@chakra-ui/theme-tools"
 import { colors, fonts, styles } from "./foundation"
 export { ChakraProvider as ThemeProvider } from "@chakra-ui/react"
 
@@ -9,7 +10,33 @@ export const theme = extendTheme({
     useSystemColorMode: false,
   },
   colors,
-  components: { Button, Heading, Input, Table, Tabs, Text, Textarea },
+  components: {
+    Table: (props: Record<string, any>) => ({
+      variants: {
+        simple: {
+          th: {
+            color: mode("gray.600", "gray.400")(props),
+            borderBottom: "1px",
+            borderColor: mode("lifted.gray.100", "lifted.gray.600")(props),
+          },
+          td: {
+            borderBottom: "1px",
+            borderColor: mode("lifted.gray.100", "lifted.gray.600")(props),
+          },
+          caption: {
+            color: mode("lifted.gray.600", "lifted.gray.100")(props),
+          },
+          tfoot: {
+            tr: {
+              "&:last-of-type": {
+                th: { borderBottomWidth: 0 },
+              },
+            },
+          },
+        },
+      }
+    }),
+  },
   fonts,
   styles,
 })
