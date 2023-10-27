8861389ad34a76d3d834c9544f606f79af9807bf
Justin Griffin <jgriffin555@comcast.net>
2023-10-27 18:02:23 -0500

    feat: Add custom class for targeting charts

--- src/theme/foundation/styles.ts
+++ src/theme/foundation/styles.ts
@@ -12,22 +12,25 @@ export const styles = {
       color: mode("black","white")(props),
       fill: mode("black","white")(props),
     },
-    ".table-lifted td": {
+    ".talib-table td": {
       borderBottomColor: mode("lifted.gray.200","lifted.gray.500",)(props),
     },
-    ".apexcharts-tooltip.apexcharts-theme-light .apexcharts-tooltip-title": {
+    ".talib-chart .apexcharts-tooltip.apexcharts-theme-light .apexcharts-tooltip-title": {
       bg: mode("lifted.gray.100","lifted.gray.800",)(props),
       borderBottomColor: mode("black","white")(props),
     },
-    ".apexcharts-tooltip-title": {
+    ".talib-chart .apexcharts-tooltip-title": {
       color: mode("black","lifted.gray.500")(props),
     },
-    ".apexcharts-tooltip.apexcharts-theme-light" : {
-      bg: mode("lifted.gray.100","lifted.gray.800",)(props),
+    ".talib-chart .apexcharts-tooltip" : {
+      bg: mode("lifted.gray.100","lifted.gray.700",)(props),
       color: mode("black","white")(props),
     },
-    ".apexcharts-tooltip-text-y-label": {
-      color: mode("black","lifted.gray.500")(props),
+    ".talib-chart .apexcharts-tooltip-text-y-label": {
+      color: mode("black","white")(props),
+    },
+    ".talib-chart .pexcharts-tooltip-text-y-value": { 
+      color: mode("black","white")(props),
     },
   }),
 };
