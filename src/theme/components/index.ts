84ea3d8ac2442ec1cd61d8b2a1554ef18d8635cd
Justin Griffin <jgriffin555@comcast.net>
2024-01-12 21:45:16 -0600

    feat: Remove code component

--- src/theme/components/index.ts
+++ src/theme/components/index.ts
@@ -5,3 +5,4 @@ export * from "./spinner"
 export * from "./tabs"
 export * from "./text"
 export * from "./textarea"
+
--- src/theme/index.ts
+++ src/theme/index.ts
@@ -1,5 +1,5 @@
 import { extendTheme } from "@chakra-ui/react"
-import { Textarea, Tabs, Button, Heading, Text, Input, Code } from ".."
+import { Textarea, Tabs, Button, Heading, Text, Input } from ".."
 import { colors, fonts, styles } from "./foundation"
 export { ChakraProvider as ThemeProvider } from "@chakra-ui/react"
 
@@ -9,7 +9,14 @@ export const theme = extendTheme({
     useSystemColorMode: false,
   },
   colors,
-  components: { Button, Heading, Input, Tabs, Text, Textarea, Code },
+  components: { 
+    Button, 
+    Heading, 
+    Input, 
+    Tabs, 
+    Text, 
+    Textarea 
+  },
   fonts,
   styles,
 })
