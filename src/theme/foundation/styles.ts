9600c486ff9877aa10c33b1fcb3fd19f598d4965
Félix C. Morency <1102868+fmorency@users.noreply.github.com>
2026-01-15 09:58:42 -0500

    feat: port from feat/color-mode (#29)

--- src/theme/foundation/styles.ts
+++ src/theme/foundation/styles.ts
@@ -1,9 +1,67 @@
+import { mode } from "@chakra-ui/theme-tools";
+
 export const styles = {
-  global: {
+  global: (props: Record<string, any>) => ({
     "html,body,#root": {
-      bgColor: "lifted.gray.50",
-      color: "black",
+      bg: mode("lifted.gray.50", "#03030f")(props),
+      bgColor: mode("lifted.gray.50", "#03030f")(props),
+      color: mode("black", "white")(props),
       height: "100%",
     },
-  },
+    text: {
+      color: mode("black", "white")(props),
+      fill: mode("black", "white")(props),
+    },
+    ".talib-table td": {
+      borderBottomColor: mode("lifted.gray.200", "lifted.gray.500")(props),
+    },
+    ".talib-chart .apexcharts-tooltip.apexcharts-theme-light .apexcharts-tooltip-title":
+      {
+        bg: mode("lifted.gray.100", "#03030f")(props),
+        borderBottomColor: mode("lifted.gray.200", "lifted.gray.500")(props),
+      },
+    ".talib-chart .apexcharts-tooltip-title": {
+      color: mode("black", "lifted.gray.500")(props),
+    },
+    ".talib-chart .apexcharts-tooltip.apexcharts-theme-light": {
+      bg: mode("lifted.gray.100", "lifted.gray.700")(props),
+      color: mode("black", "white")(props),
+      borderColor: mode("lifted.gray.200", "lifted.gray.500")(props),
+    },
+    ".talib-chart .apexcharts-tooltip-text-y-label": {
+      color: mode("black", "white")(props),
+    },
+    ".talib-chart .apexcharts-tooltip-text-y-value": {
+      color: mode("black", "white")(props),
+    },
+    ".metric-nav a:hover": {
+      color: "brand.teal.500",
+      textDecoration: "none",
+      cursor: "pointer",
+    },
+    ".metric-nav .active": {
+      color: "brand.teal.500",
+      fontWeight: "bold",
+      textDecoration: "none",
+    },
+    ".top-nav a": {
+      fontSize: "20px",
+      lineHeight: "47px",
+      paddingRight: "5px",
+    },
+    ".top-nav a:hover": {
+      color: "brand.teal.500",
+      textDecoration: "none",
+      cursor: "pointer",
+    },
+    ".top-nav .active": {
+      color: "brand.teal.500",
+      textDecoration: "none",
+    },
+    ".top-nav-dropdown a:hover": {
+      color: "brand.teal.500",
+      textDecoration: "none",
+      cursor: "pointer",
+    },
+  }),
 };
